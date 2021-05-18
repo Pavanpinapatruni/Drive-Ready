ee68f40f6f98459e516a47eb9a5e55484974712b
Pavanpinapatruni <61941859+Pavanpinapatruni@users.noreply.github.com>
2021-05-18 15:09:40 +0530

    Add files via upload

---
+++ fib_position.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int fib_pos(int n)
+{
+	int a=0,b=1,c=1,i=2;
+	if(n==0)
+	return 1;
+	while(c<=n)
+	{
+		if(c==n)
+		return i;
+		c=a+b;
+		a=b;
+		b=c;
+		i++;
+		
+	}
+	
+	return 0;
+}
+int main()
+{
+	int n,k;
+	scanf("%d",&n);
+	k=fib_pos(n);
+	if(k!=0)
+	printf("%d",k);
+	else
+	printf("False");
+	return 0;
+}
